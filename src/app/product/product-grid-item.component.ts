0cef9cd236d68ddea8abb593bdbcfecf8c31ab72
poyers <poyers_38@yahoo.com>
2018-06-18 14:48:51 +0800

    3rd Breadcrumb modification

--- src/app/product/product-grid-item.component.ts
+++ src/app/product/product-grid-item.component.ts
@@ -33,6 +33,6 @@ export class ProductGridItemComponent implements OnInit {
 	}
 
 	onClicked(prodDetailUrl: string) {
-		this.router.navigate(['shop/' + prodDetailUrl + '/' + this.product.id]);
+		this.router.navigate(['shop sri/' + prodDetailUrl + '/' + this.product.id]);
 	}
 }
